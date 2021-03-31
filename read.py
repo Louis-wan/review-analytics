6a9b0ee57e03b78f3fd1de46874d9b70dffa5c3a
Louis-wan <louis10508@gmail.com>
2021-03-31 07:21:39 -0700

    2nd version add length

--- read.py
+++ read.py
@@ -1,12 +1,17 @@
 data = []
-count = 0
-with open('reviews.txt', 'r') as f:
-	for line in f:
-		data.append(line)
-		count = count +1
-		if count % 1000 == 0:#餘數
-			print(len(data))
-print(len(data)) 
+with open ('reviews.txt', 'r') as f :
+	for line in f :
+		data.append (line)
+#print(len(data))
+#print(data) 顯示全部的比數
+print(data[0])
+print('------------------')
+print(data[1])
 
-print(data[3])
-print(data[5])
+
+sum_len=0
+for d in data:
+	sum_len = sum_len + len(d)
+print ('留言的平均長度', sum_len/len(data))
+# 讀取data 架構 
+#需要再理解一下 在length 中 長data  跟在 最外層不同
